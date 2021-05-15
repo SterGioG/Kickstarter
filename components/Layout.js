c7a2c92a202f75a9e265fe229d73b7009cfd855e
SterGioG <83977607+SterGioG@users.noreply.github.com>
2021-05-14 19:01:44 -0500

    Add files via upload

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Container } from 'semantic-ui-react';
+import Header from './Header';
+import Head from 'next/head';
+ 
+const Layout = (props) => {
+  return (
+    <Container>
+        <Head>
+            <link
+            async
+            rel="stylesheet"
+            href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.0/dist/semantic.min.css"
+            />
+        </Head>
+        <Header />
+        {props.children}
+    </Container>
+  );
+};
+
+export default Layout;
